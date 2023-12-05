de98cf1fec51b9f8c6e75122481baf3dc2a1216d
SosMrRaziel <lbt_alh@hotmail.fr>
2023-12-05 21:11:42 +0100

    task 3 unfixed

--- models/base_model.py
+++ models/base_model.py
@@ -4,23 +4,24 @@ from datetime import datetime
 
 
 class BaseModel:
-    def __init__(self, id, created_at, updated_at):
-        self.id = id
-        self.created_at = created_at
-        self.updated_at = updated_at
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
-        id = str(uuid.uuid4())
-        
-        updated_at = datetime.now()
 
     def __str__(self):
-        return "[BaseModel] {} {}".format(id = self.id, self.__dict__)
+        return "{} {} {}".format(self.__class__.__name__ ,id = self.id, self.__dict__)
     
     def save(self):
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        dic = "BaseModel(id={self.id}, created_at={self.created_at}, updated_at={self.updated_at})".format(())
+        dic = self.__dict__.copy()
+        dic["__class__"] = self.__class__.__name__
+        dic["created_at"] = self.created_at.isoformat()
+        dic["updated_at"] = self.updated_at.isoformat()
+        return dic
 
 
 
@@ -53,20 +54,4 @@ class BaseModel:
 
 
 
-
-
-
-
-#     def __repr__(self):
-#         return f"BaseModel(id={self.id}, created_at={self.created_at}, updated_at={self.updated_at})"
-    
-# id = str(uuid.uuid4())
-# created_at = datetime.now()
-# updated_at = datetime.now()
-
-# base_model = BaseModel(id, created_at, updated_at)
-# print(base_model)
-
-# base_model1 = BaseModel(id, created_at, updated_at)
-# print(base_model1.__dict__)
 
