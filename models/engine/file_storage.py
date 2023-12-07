37010b1ee01965df0f281e0b631ddd959966aaa6
Emran <ouakouriimran@gmail.com>
2023-12-07 18:20:47 +0100

    we are enjoying our first console

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,12 @@
 import json
 from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.amenity import Amenity
+from models.city import City
+
 
 class FileStorage:
     __file_path = "file.json"
