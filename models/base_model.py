7c11390caaed3195c1849ec04c5e59feea12764d
Emran <ouakouriimran@gmail.com>
2023-12-09 13:27:16 +0100

    recommenting

--- models/base_model.py
+++ models/base_model.py
@@ -20,6 +20,7 @@ class BaseModel:
             for key, value in kwargs.items():
                 if key != '__class__':
                     if key in ('created_at', 'updated_at'):
+                        # Convert string to datetime object
                         value = datetime.strptime(value,
                                                   '%Y-%m-%dT%H:%M:%S.%f')
                     setattr(self, key, value)
