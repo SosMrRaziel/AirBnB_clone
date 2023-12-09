8519c9b0780024cf32b4d2e4e7b5e8194c909da1
SosMrRaziel <lbt_alh@hotmail.fr>
2023-12-09 21:38:01 +0100

    test_file_storage

--- tests/test_engine/test_file_storage.py
+++ tests/test_engine/test_file_storage.py
@@ -3,60 +3,9 @@ import tempfile
 import json
 from models.engine.file_storage import FileStorage
 
-""" Define some sample classes and objects to test the FileStorage class """
-class BaseModel:
-    """
-    A base class for all models.
+""" Define some sample classes and
+objects to test the FileStorage class """
 
-    Attributes:
-        id (int): The unique identifier of the model.
-        name (str): The name of the model.
-    """
-
-    def __init__(self, id, name):
-        """
-        Initializes a new model with the given id and name.
-
-        Args:
-            id (int): The unique identifier of the model.
-            name (str): The name of the model.
-        """
-        self.id = id
-        self.name = name
-
-    def to_dict(self):
-        """
-        Returns a dictionary representation of the model.
-
-        Returns:
-            dict: A dictionary containing the attributes of the model.
-        """
-        return self.__dict__
-
-class User(BaseModel):
-    """
-    A class that represents a user.
-
-    Attributes:
-        id (int): The unique identifier of the user.
-        name (str): The name of the user.
-        email (str): The email address of the user.
-    """
-
-    def __init__(self, id, name, email):
-        """
-        Initializes a new user with the given id, name, and email.
-
-        Args:
-            id (int): The unique identifier of the user.
-            name (str): The name of the user.
-            email (str): The email address of the user.
-        """
-        super().__init__(id, name)
-        self.email = email
-
-user1 = User(1, "Alice", "alice@example.com")
-user2 = User(2, "Bob", "bob@example.com")
 
 class FileStorageTest(unittest.TestCase):
     """
@@ -87,7 +36,8 @@ class FileStorageTest(unittest.TestCase):
         """
         Tests the new method of the FileStorage class.
 
-        Checks that the new method adds the given objects to the __objects dictionary
+        Checks that the new method adds
+        the given objects to the __objects dictionary
         with the correct keys and values.
         """
         # Add two objects to the file storage
@@ -104,7 +54,8 @@ class FileStorageTest(unittest.TestCase):
         """
         Tests the save method of the FileStorage class.
 
-        Checks that the save method serializes the __objects dictionary to the JSON file
+        Checks that the save method serializes
+        the __objects dictionary to the JSON file
         with the correct data format.
         """
         # Add two objects to the file storage
@@ -136,7 +87,8 @@ class FileStorageTest(unittest.TestCase):
         """
         Tests the reload method of the FileStorage class.
 
-        Checks that the reload method deserializes the JSON file to the __objects dictionary
+        Checks that the reload method deserializes
+        the JSON file to the __objects dictionary
         with the correct object instances.
         """
         # Write some data to the JSON file
@@ -163,10 +115,13 @@ class FileStorageTest(unittest.TestCase):
         self.assertIn("User.2", self.file_storage.all())
         self.assertEqual(self.file_storage.all()["User.1"].id, 1)
         self.assertEqual(self.file_storage.all()["User.1"].name, "Alice")
-        self.assertEqual(self.file_storage.all()["User.1"].email, "alice@example.com")
+        self.assertEqual(self.file_storage.all()["User.1"].email,
+                         "alice@example.com")
         self.assertEqual(self.file_storage.all()["User.2"].id, 2)
         self.assertEqual(self.file_storage.all()["User.2"].name, "Bob")
-        self.assertEqual(self.file_storage.all()["User.2"].email, "bob@example.com")
+        self.assertEqual(self.file_storage.all()["User.2"].email,
+                         "bob@example.com")
+
 
 if __name__ == "__main__":
     unittest.main()
