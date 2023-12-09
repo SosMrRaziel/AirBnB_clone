1429e48b934b39739a012a6a107f214eb4645339
Emran <ouakouriimran@gmail.com>
2023-12-09 13:56:25 +0100

    back to normal

--- models/base_model.py
+++ models/base_model.py
@@ -1,80 +1,60 @@
 #!/usr/bin/python3
+
 import uuid
 from datetime import datetime
 import models
 
-"""
-defines all common attributes/methods for other classes
-"""
+"""define BaseModl class"""
 
 
 class BaseModel:
-    """ The BaseModel class defines common attributes and methods that can be
-    inherited by other classes in the application.
-
-    Attributes:
-        id (str): A universally unique identifier (UUID) for the object.
-        created_at (datetime): indicating the object's creation time.
-        updated_at (datetime): indicating the object's last update time.
-
-    Methods:
-        __init__: Initializes a new instance of the BaseModel class.
-        __str__: Returns a string representation of the object.
-        save: Updates the 'updated_at' attribute to the current timestamp.
-        to_dict: Converts the object to a dictionary format.
-    """
+    """BaseModel class that defines common
+            attributes and methods for other classes"""
 
     def __init__(self, *args, **kwargs):
-        """
-        Initializes a new instance of the BaseModel class.
+        """Initialize a new BaseModel instance
 
-        Args:
-            *args: Variable-length argument list.
-            **kwargs: Arbitrary keyword arguments.
+        Attributes:
+            id (str): a unique identifier for the instance
+            created_at (datetime): the creation date and time of the instance
+            updated_at (datetime): the last update date and time of th instance
         """
 
         if kwargs:
             for key, value in kwargs.items():
-                if key != "__class__":
-                    if key == "created_at" or key == "updated_at":
-                        date_format = "%Y-%m-%dT%H:%M:%S.%f"
-                        self.__dict__[key] = datetime.strptime(
-                                value, date_format)
-                    else:
-                        self.__dict__[key] = value
+                if key != '__class__':
+                    if key in ('created_at', 'updated_at'):
+                        value = datetime.strptime(value,
+                                                  '%Y-%m-%dT%H:%M:%S.%f')
+                    setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.created_at = self.updated_at = datetime.now()
             models.storage.new(self)
 
     def __str__(self):
-        """
-        Returns a string representation of the object.
+        """Return a string representation of the instance"""
 
-        Returns:
-            str: A string containing the class name, id
-        """
         return "[{}] ({}) {}".format(
-                self.__class__.__name__, self.id, self.__dict__)
+                    self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """
-        Updates the 'updated_at' attribute to the current timestamp.
-        """
+        """Update the updated_at attribute with the current datetime"""
 
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
-        """
-        Converts the object to a dictionary format.
+        """Return a dictionary representation of the instance
 
         Returns:
-            dict: A dictionary representation of the object.
+            dict: a dictionary containing all the attributes and their values,
+                with the class name under the key "__class__" and the datetime
+                objects converted to ISO format strings
         """
-        obj = self.__dict__.copy()
-        obj['__class__'] = self.__class__.__name__
-        obj['created_at'] = self.created_at.isoformat()
-        obj['updated_at'] = self.updated_at.isoformat()
-        return obj
+
+        dic = self.__dict__.copy()
+        dic["__class__"] = self.__class__.__name__
+        dic["created_at"] = self.created_at.isoformat()
+        dic["updated_at"] = self.updated_at.isoformat()
+        return dic
