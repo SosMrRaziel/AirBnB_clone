c39024935bdb24ebd63906d7f26fe21eb5a94814
Emran <ouakouriimran@gmail.com>
2023-12-10 22:43:39 +0100

    test task 5

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import unittest
+from models.engine.file_storage import FileStorage
+import models
+from models.base_model import BaseModel
+from models.user import User
+
+
+class TestFilesStorage(unittest.TestCase):
+    """a testing of all possible cases .. not all but most of them"""
+
+    def test_instance(self):
+        self.assertIs(FileStorage, type(FileStorage()))
+        self.assertIsInstance(FileStorage.__objects, dict)
+        self.assertIsInstance(FileStorage.__file_path, str)
+    
+    def test_NewStor(self):
+        models.storage.new(BaseModel())
+        models.storage.new(User())
+        self.assertIn("BaseModel.{}".format(BaseModel().id),
+                      models.storage.all().keys())
+        self.assertIn("User.{}".format(User().id), models.storage.all().keys())
+
+    def test_AllStorage(self):
+        self.assertEqual(type(models.storage.all()), dict)
