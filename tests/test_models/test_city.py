968e5e1707074e0c41cfe87cbfe222e7508dae79
Emran <ouakouriimran@gmail.com>
2023-12-10 22:56:19 +0100

    test task 5

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -30,5 +30,6 @@ class TestCity(unittest.TestCase):
         self.assertIsInstance(city.name, str)
         self.assertIsInstance(city.state_id, str)
 
+
 if __name__ == '__main__':
     unittest.main()
