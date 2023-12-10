c31a2a06172037955f309dceee619dddbca3c374
Emran <ouakouriimran@gmail.com>
2023-12-10 23:33:06 +0100

    test task 9

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,7 +1,7 @@
 import unittest
 from models.base_model import BaseModel as BS
 from models.review import Review
-
+from datetime import datetime
 
 class TestReview(unittest.TestCase):
     """A class to test the Review class."""
@@ -17,13 +17,16 @@ class TestReview(unittest.TestCase):
         self.assertTrue(hasattr(review, "user_id"))
         self.assertTrue(hasattr(review, "text"))
 
-    def test_review_attributes_are_correct_types(self):
+    def test_isntance(self):
         # check that Review attributes have the correct types
         review = Review()
         review.place_id = "123"
         review.user_id = "456"
         review.text = "This place is awesome"
         self.assertIsInstance(review.place_id, str)
+        self.assertIsInstance(review.id, str)
+        self.assertIsInstance(review.created_at, datetime)
+        self.assertIsInstance(review.updated_at, datetime)
         self.assertIsInstance(review.user_id, str)
         self.assertIsInstance(review.text, str)
 
